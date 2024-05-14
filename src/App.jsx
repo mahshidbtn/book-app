cdaf5acd3052c0ed49fc3902693f6926a3fb6b8d
mahshidbtn <mahshid.bateni@yahoo.com>
2024-05-14 12:17:21 +0330

    create bookCard components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Books from "./components/Books";
 import Layout from "./layouts/Layout";
 
 function App() {
   return (
     <>
       <Layout>
+        <Books />
       </Layout>
     </>
   );
